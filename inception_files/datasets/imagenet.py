dc8e1c9de7cc72f335533760940c77a81a841d61
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-05-29 02:27:01 +0200

    Gitignore Modified and Repo Cleaned

--- inception_files/datasets/imagenet.py
+++ inception_files/datasets/imagenet.py
@@ -36,7 +36,7 @@ import os
 from six.moves import urllib
 import tensorflow as tf
 
-from inception_files.datasets import dataset_utils
+from datasets import dataset_utils
 
 slim = tf.contrib.slim
 
