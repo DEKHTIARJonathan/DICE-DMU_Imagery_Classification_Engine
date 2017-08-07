f93ffcb282518cdf0d9979f2bc6005be7b449873
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-08-07 14:03:36 +0200

    updated with latest model

--- tf_classifier.py
+++ tf_classifier.py
@@ -16,11 +16,15 @@ slim            = tf.contrib.slim
 IMAGE_SIZE      = inception_v1.default_image_size
 
 MODEL_REPO_URL  = "https://raw.githubusercontent.com/DEKHTIARJonathan/DICE-DMU_Imagery_Classification_Engine/web-api"
-MODEL_PATH      = "inception_files/models"
-MODEL_NAME      = "inception_v1.ckpt"
 
-MODEL_FILEPATH  = MODEL_PATH + "/" + MODEL_NAME
-MODEL_URL       = MODEL_REPO_URL + "/" + MODEL_PATH + "/" +MODEL_NAME
+MODELDIR_PATH      = os.path.join("inception_files", "models")
+
+MODEL_NAME      = "dice_dmunet.ckpt"
+MODEL_FILEPATH  = os.path.join(MODELDIR_PATH, MODEL_NAME)
+MODEL_URL       = os.path.join(MODEL_REPO_URL, MODELDIR_PATH, MODEL_NAME)
+
+LABEL_FILE_NAME = "dmunet_labels.txt"
+LABEL_FILE_PATH  = os.path.join(MODELDIR_PATH, LABEL_FILE_NAME)
 
 JPEG_EXT_LIST   = ["jpg", "jpeg"]
 
@@ -31,19 +35,31 @@ class Tensorflow_ImagePredictor():
     ext_plh           = tf.placeholder(tf.string)
     img_plh           = tf.placeholder(tf.string, shape=None)
 
-    names             = imagenet.create_readable_names_for_imagenet_labels()
+    names             = None
 
     def __init__(self):
         print("Tensorflow_ImagePredictor: Model Checking Starting ...")
 
-        if not os.path.isdir(MODEL_PATH):
-            os.makedirs(MODEL_PATH)
+        if not os.path.isdir(MODELDIR_PATH):
+            os.makedirs(MODELDIR_PATH)
 
         if not os.path.isfile(MODEL_FILEPATH):
             urllib.request.urlretrieve (MODEL_URL, MODEL_FILEPATH)
 
-        print("Tensorflow_ImagePredictor: Model Checking Finished ...\n")
+        if not os.path.isfile(LABEL_FILE_PATH):
+            self.names = imagenet.create_readable_names_for_imagenet_labels()
+        else:
+            labels = open(LABEL_FILE_PATH, 'r')
 
+            #Create a dictionary to refer each label to their string name
+            self.names = {}
+            for line in labels:
+                label, string_name = line.split(':')
+                string_name = string_name[:-1] #Remove newline
+                self.names[int(label)] = string_name
+
+        print("Tensorflow_ImagePredictor: Model Checking Finished ...\n")
+        
         ########################################################################
 
         print("Tensorflow_ImagePredictor: Initialisation Starting ...")
@@ -53,14 +69,15 @@ class Tensorflow_ImagePredictor():
         self.file_cond         = tf.equal(self.file_cond, 1) ## 1 => JPEG EXTENSION, 0 => PNG EXTENSION
 
         self.image             = tf.cond(
-                                 self.file_cond,
-                                 lambda: tf.image.decode_jpeg(self.img_plh, channels=3),
-                                 lambda: tf.image.decode_png(self.img_plh, channels=3)
-                            )
+                                     self.file_cond,
+                                     lambda: tf.image.decode_jpeg(self.img_plh, channels=3),
+                                     lambda: tf.image.decode_png(self.img_plh, channels=3)
+                                )
 
         self.processed_image   = inception_preprocessing.preprocess_image(self.image, IMAGE_SIZE, IMAGE_SIZE, is_training=False)
         self.processed_images  = tf.expand_dims(self.processed_image, 0)
-
+        
+        
         #Config is necessary in order to prevent windows GPU compute failure
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -68,15 +85,15 @@ class Tensorflow_ImagePredictor():
 
         # Create the model, use the default arg scope to configure the batch norm parameters.
         with slim.arg_scope(inception_v1_arg_scope()):
-            self.logits, self._ = inception_v1(self.processed_images, num_classes=1001, is_training=False)
-
+            self.logits, self._ = inception_v1(self.processed_images, num_classes=len(self.names), is_training=False)
+        
         self.probabilities = tf.nn.softmax(self.logits)
 
         self.init_fn           = slim.assign_from_checkpoint_fn(
                                     MODEL_FILEPATH,
                                     slim.get_variables_to_restore()
                                  )
-
+        
         self.init_fn(self.sess)
 
         print("Tensorflow_ImagePredictor: Initialisation Finished ...")
@@ -105,3 +122,4 @@ class Tensorflow_ImagePredictor():
             payload["results"].append(tmp)
 
         return payload
+
